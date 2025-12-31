8bba371c9f42e435a495cc1664293c8b5c1a7e0f
Osas997 <larasatyt83@gmail.com>
2025-12-31 18:02:48 +0700

    change umkm endpoint to public

--- src/modules/umkm/umkm.controller.ts
+++ src/modules/umkm/umkm.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { UmkmService } from './providers/umkm.service';
 import { baseResponse } from 'src/helpers/base-response';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('umkm')
 export class UmkmController {
   constructor(private readonly umkmService: UmkmService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Get()
   getJson() {
     const data = this.umkmService.getJson();
